bc2a770dc8bb9b2b2832302b4f085bd0b4f940d9
Jardynero <egor.vnl@gmail.com>
2021-11-18 11:25:18 +0300

    add appbar

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_dym/functions/firebase_functions.dart';
+import 'package:pizza_dym/global_widgets/appBar.dart';
 import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -15,6 +16,7 @@ class ProfileScreen extends StatelessWidget {
     final _auth =
         Provider.of<FirebaseAuthInstance>(context, listen: false).auth;
     return Scaffold(
+      appBar: MainAppBar(_title),
       body: Center(
         child: ElevatedButton(
           onPressed: () => _auth.signOut(),
