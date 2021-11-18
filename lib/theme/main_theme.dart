d018e0b0c217e3156dffd3cc8129cdbb7a52085e
Jardynero <egor.vnl@gmail.com>
2021-11-18 07:34:57 +0300

    main page theme

--- lib/theme/main_theme.dart
+++ lib/theme/main_theme.dart
@@ -1,40 +1,25 @@
 import 'package:flutter/material.dart';
 
 class MainColorTheme {
-  Color scaffoldBackgroundColor = Color.fromRGBO(39, 40, 42, 1.0);
-  Color navBackgroundColor = Colors.black;
-  Color primaryColor = Color.fromRGBO(255, 120, 41, 1.0);
-  Color fontColor = Color.fromRGBO(255, 255, 255, 1.0);
-  double paragraphFontSize = 17.0;
-  double secondaryFontSize = 15.0;
-
   ThemeData mainTheme = ThemeData(
     brightness: Brightness.light,
-    scaffoldBackgroundColor: Color(0xffE5E5E5),
+    scaffoldBackgroundColor: Color(0xffffffff),
     appBarTheme: AppBarTheme(
-      backgroundColor: Color(0xff27282A),
-      elevation: 0,
-    ),
-    tabBarTheme: TabBarTheme(
-      labelPadding: EdgeInsets.symmetric(horizontal: 20),
-      unselectedLabelColor: Color(0xffffffff),
-      indicatorSize: TabBarIndicatorSize.label,
-      indicator: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Color(0xffFF9F38),
-      )
+      iconTheme: IconThemeData(color: Colors.grey, size: 30),
+      backgroundColor: Color(0xffffffff),
+      titleTextStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 20),
+      elevation: 4,
     ),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: Color(0xff000000),
         selectedItemColor: Color(0xffFF9F38)),
-        unselectedWidgetColor: Color(0xffffffff),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         primary: Color(0xffFF9F38),
+        textStyle: TextStyle(fontSize: 14 ,fontWeight: FontWeight.w500,),
       ),
     ),
     textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(primary: Color(0xffFF7829))),
+        style: TextButton.styleFrom(primary: Color(0xffFF9F38))),
     progressIndicatorTheme: ProgressIndicatorThemeData(
       color: Colors.black,
     ),
