154799f654012e7e9314935cc506afd8a3af762a
Jardynero <egor.vnl@gmail.com>
2021-11-18 11:25:32 +0300

    add appbar

--- lib/screens/index_screen/index_screen.dart
+++ lib/screens/index_screen/index_screen.dart
@@ -5,6 +5,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_dym/functions/firebase_functions.dart';
 import 'package:pizza_dym/global_widgets/appBar-actions.dart';
+import 'package:pizza_dym/global_widgets/appBar.dart';
+import 'package:pizza_dym/models/customer-data_model.dart';
 
 // Import screens
 import 'package:pizza_dym/screens/menu_screen/menu_screen.dart';
@@ -19,20 +21,15 @@ class IndexScreen extends StatelessWidget {
     final _auth =
         Provider.of<FirebaseAuthInstance>(context, listen: false).auth;
 
+    final _title = Provider.of<CustomerData>(context, listen: false).name;
+
     _auth.authStateChanges().listen((User? user) {
       if (user == null) {
         Navigator.pushNamed(context, '/login-phone');
       }
     });
     return Scaffold(
-      appBar: AppBar(
-        elevation: 4,
-        title: Text('Пиццерия Дым'),
-        centerTitle: true,
-        actions: [
-          profileAppBarAction(context),
-        ],
-      ),
+      appBar: MainAppBar('$_title'),
       body: MenuScreen(),
     );
   }
