46d4c77e597694ef2d7b8284f43f99f48a96c865
Jardynero <egor.vnl@gmail.com>
2021-11-18 11:25:05 +0300

    add appbar

--- lib/screens/about-pizza_screen.dart
+++ lib/screens/about-pizza_screen.dart
@@ -1,13 +1,17 @@
 // About pizza page
 
 import 'package:flutter/material.dart';
+import 'package:pizza_dym/global_widgets/appBar.dart';
 
 class AboutPizzaScreen extends StatelessWidget {
   const AboutPizzaScreen({Key? key}) : super(key: key);
 
+  final String _title = 'О нас';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: MainAppBar('$_title'),
       body: Center(child: Text('О нас'),)
     );
   }
