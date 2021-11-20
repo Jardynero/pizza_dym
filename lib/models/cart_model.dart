a19777f8154f018b1537c5873f0f800af8d6fc23
Jardynero <egor.vnl@gmail.com>
2021-11-21 00:00:39 +0300

    add cart logic

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,7 +1,34 @@
 // cart model
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_cart/flutter_cart.dart';
 
-class CartModel extends ChangeNotifier{
+class CartModel extends ChangeNotifier {
+  // Переменная содержить все позиции закаха
+  // _orderList[[0]] = Название позиции
+  // _orderList[[1]] = количество данного товара
+  // _orderList[[2]] = стоимость товара
+  // _orderList[[3]] = фото товара
   
+  var cart = FlutterCart();
+
+  double get getTotalItemsPrice => cart.getTotalAmount();
+
+  void addProductToCart(productId, unitPrice, productName,) {
+    cart.addToCart(productId: productId, unitPrice: unitPrice, productName: productName);
+
+    notifyListeners();
+  }
+
+  void incrementItemToCart(itemId) {
+    cart.incrementItemToCart(cart.findItemIndexFromCart(itemId)!);
+
+    notifyListeners(); 
+  }
+
+  void decrementItemFromCart(itemId) {
+    cart.decrementItemFromCart(cart.findItemIndexFromCart(itemId)!);
+
+    notifyListeners();
+  }
 }
