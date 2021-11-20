c4bd3df34eb7c7956c8ceff7e32b19428c5814e3
Jardynero <egor.vnl@gmail.com>
2021-11-20 23:58:17 +0300

    add FAB

---
+++ lib/global_widgets/floating-action-btn.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_dym/models/cart_model.dart';
+import 'package:provider/provider.dart';
+
+class FloatingActionBtn extends StatelessWidget {
+  const FloatingActionBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Общая стоимость всех товаров в корзине
+    double _totalItemsPrice = Provider.of<CartModel>(context, listen: true).getTotalItemsPrice;
+
+    return Visibility(
+      visible: _totalItemsPrice == 0? false : true,
+      child: Container(
+        margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 100 * 8),
+        child: FloatingActionButton.extended(
+          onPressed: () => Navigator.pushNamed(context, '/cart'),
+          icon: Icon(Icons.shopping_bag_outlined),
+          label: Text('${Provider.of<CartModel>(context, listen: true).getTotalItemsPrice.toInt()}₽', style: TextStyle(fontSize: 18,)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 10,
+          backgroundColor: Color(0xff27282A),
+          tooltip: 'Корзина',
+        ),
+      ),
+    );
+  }
+}
