9ebdc19382dfecfe01b09bd5d8754612ac2b2b8c
Jardynero <egor.vnl@gmail.com>
2021-11-20 23:59:24 +0300

    item list

--- lib/screens/menu_screen/widgets/item-list_screen.dart
+++ lib/screens/menu_screen/widgets/item-list_screen.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:pizza_dym/global_widgets/appBar-actions.dart';
 import 'package:pizza_dym/global_widgets/appBar.dart';
+import 'package:pizza_dym/global_widgets/floating-action-btn.dart';
+import 'package:pizza_dym/models/cart_model.dart';
+import 'package:provider/provider.dart';
 
 class ItemListScreen extends StatelessWidget {
   final String categorieName;
@@ -15,8 +17,10 @@ class ItemListScreen extends StatelessWidget {
         _firestore.collection('restaurant').doc('menu').snapshots();
     int _counter = 1;
     Map _menuItemsSorted = {};
+    List<List> _menuItemsSortedList = [];
     return Scaffold(
       appBar: MainAppBar(categorieName),
+      floatingActionButton: FloatingActionBtn(),
       body: StreamBuilder(
         stream: _menuItems,
         builder:
@@ -24,36 +28,51 @@ class ItemListScreen extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          sortMenuItems(_counter, snapshot, _menuItemsSorted);
-          return ListView(
-            children: [
-              for (final item in _menuItemsSorted.values)
-                if (item[3] == categorieName) ItemCard(item),
-            ],
+          sortMenuItems(
+              _counter, snapshot, _menuItemsSorted, _menuItemsSortedList);
+          return ListView.builder(
+            itemCount: _menuItemsSortedList.length,
+            itemBuilder: (BuildContext context, index) {
+              if (_menuItemsSortedList[index][3] == categorieName) {
+                return ItemCard(_menuItemsSortedList[index]);
+              } else {
+                return SizedBox.shrink();
+              }
+            },
           );
         },
       ),
     );
   }
 
-  void sortMenuItems(_counter, snapshot, _menuItemsSorted) {
+  void sortMenuItems(
+      _counter, snapshot, _menuItemsSorted, List _menuItemsSortedList) {
     while (_counter != snapshot.data!.get('menu').length + 1) {
       _menuItemsSorted[_counter] = snapshot.data!.get('menu')['$_counter'];
       _counter++;
     }
+    for (final item in _menuItemsSorted.values) {
+      _menuItemsSortedList.add(item);
+    }
   }
 }
 
-class ItemCard extends StatelessWidget {
+class ItemCard extends StatefulWidget {
   final _itemData;
   ItemCard(this._itemData, {Key? key}) : super(key: key);
 
   @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  @override
   Widget build(BuildContext context) {
-    final String itemName = _itemData[0];
-    final String itemPhotoUrl = _itemData[4];
-    final int itemCost = _itemData[2];
-    final bool availableToBuy = _itemData[5];
+    final String itemName = widget._itemData[0];
+    final String itemPhotoUrl = widget._itemData[4];
+    final int itemCost = widget._itemData[2];
+    final bool availableToBuy = widget._itemData[5];
+    var cart = Provider.of<CartModel>(context, listen: true).cart;
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 200,
@@ -82,59 +101,83 @@ class ItemCard extends StatelessWidget {
                             fontSize: 20, fontWeight: FontWeight.w500),
                       ),
                     ),
-                    addToCartBtn(itemCost, availableToBuy),
+                    if (cart.findItemIndexFromCart(itemName) == null)
+                      SizedBox(
+                        width: 165,
+                        height: 45,
+                        child: Container(
+                          child: ElevatedButton(
+                            onPressed: availableToBuy == false
+                                ? null
+                                : () {
+                                    Provider.of<CartModel>(
+                                      context,
+                                      listen: false,
+                                    ).addProductToCart(
+                                      itemName,
+                                      itemCost,
+                                      itemName,
+                                    );
+                                  },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            child: Text(
+                              availableToBuy == false
+                                  ? 'Только в кафе'
+                                  : '$itemCost₽',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    else
+                      SizedBox(
+                        width: 165,
+                        height: 45,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Color(0xffFF9F38)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Provider.of<CartModel>(context, listen: false)
+                                      .decrementItemFromCart(itemName);
+                                },
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              Text(
+                                  '${cart.getSpecificItemFromCart(itemName)!.quantity}',
+                                  style: TextStyle(fontSize: 18)),
+                              TextButton(
+                                onPressed: () {
+                                  Provider.of<CartModel>(context, listen: false)
+                                      .incrementItemToCart(itemName);
+                                },
+                                child:
+                                    Text('+', style: TextStyle(fontSize: 18)),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                   ],
                 ),
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget selectQntBtn() {
-    return SizedBox(
-      width: 140,
-      height: 45,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Color(0xffFF9F38)),
-        ),
-        child: Row(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: Text('-', style: TextStyle(fontSize: 18)),
-            ),
-            Text('1', style: TextStyle(fontSize: 18)),
-            TextButton(
-              onPressed: () {},
-              child: Text('+', style: TextStyle(fontSize: 18)),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget addToCartBtn(itemCost, availableToBuy) {
-    return SizedBox(
-      width: 165,
-      height: 45,
-      child: Container(
-        child: ElevatedButton(
-          onPressed: availableToBuy == false ? null : () {},
-          style: ElevatedButton.styleFrom(
-            elevation: 0,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          ),
-          child: Text(
-            availableToBuy == false ? 'Только в кафе' : '$itemCost₽',
-            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
-          ),
         ),
       ),
     );
