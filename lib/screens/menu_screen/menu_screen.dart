df5da7478e4f83c89d47d8983f71da1366a83704
Jardynero <egor.vnl@gmail.com>
2021-11-25 15:11:19 +0300

    Добавил initState && убрал ненужные импорты

--- lib/screens/menu_screen/menu_screen.dart
+++ lib/screens/menu_screen/menu_screen.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_dym/functions/firebase_functions.dart';
 import 'package:pizza_dym/screens/menu_screen/widgets/cardCategories.dart';
+import 'package:provider/provider.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  void initState() {
+    CloudFirestore().isRestaurantOpen(context);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return CardCategories();
