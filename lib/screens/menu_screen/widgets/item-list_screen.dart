00bffa05ea990a520d48de65b2f664dd9a7dda08
Jardynero <egor.vnl@gmail.com>
2021-11-25 15:11:55 +0300

    добавил кликабельность карточкам && убрал импорты

--- lib/screens/menu_screen/widgets/item-list_screen.dart
+++ lib/screens/menu_screen/widgets/item-list_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:pizza_dym/global_widgets/appBar.dart';
 import 'package:pizza_dym/global_widgets/floating-action-btn.dart';
 import 'package:pizza_dym/models/cart_model.dart';
+import 'package:pizza_dym/screens/single-product_screen.dart';
 import 'package:provider/provider.dart';
 
 class ItemListScreen extends StatelessWidget {
@@ -73,111 +74,123 @@ class _ItemCardState extends State<ItemCard> {
     final int itemCost = widget._itemData[2];
     final bool availableToBuy = widget._itemData[5];
     var cart = Provider.of<CartModel>(context, listen: true).cart;
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 200,
-      child: Card(
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        elevation: 7.0,
-        shadowColor: Color(0xffEBEBEB),
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            SizedBox(
-                width: 170, height: 170, child: Image.network('$itemPhotoUrl')),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Flexible(
-                      child: Text(
-                        '$itemName',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    if (cart.findItemIndexFromCart(itemName) == null)
-                      SizedBox(
-                        width: 165,
-                        height: 45,
-                        child: Container(
-                          child: ElevatedButton(
-                            onPressed: availableToBuy == false
-                                ? null
-                                : () {
-                                    Provider.of<CartModel>(
-                                      context,
-                                      listen: false,
-                                    ).addProductToCart(
-                                      itemName,
-                                      itemCost,
-                                      itemName,
-                                    );
-                                  },
-                            style: ElevatedButton.styleFrom(
-                              elevation: 0,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                            ),
-                            child: Text(
-                              availableToBuy == false
-                                  ? 'Только в кафе'
-                                  : '$itemCost₽',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SingleProductScreen(widget._itemData),
+        ),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          elevation: 7.0,
+          shadowColor: Color(0xffEBEBEB),
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                  width: 170,
+                  height: 170,
+                  child: Image.network('$itemPhotoUrl')),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          '$itemName',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
                         ),
-                      )
-                    else
-                      SizedBox(
-                        width: 165,
-                        height: 45,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Color(0xffFF9F38)),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Provider.of<CartModel>(context, listen: false)
-                                      .decrementItemFromCart(itemName);
-                                },
-                                child: Text(
-                                  '-',
-                                  style: TextStyle(fontSize: 18),
+                      ),
+                      if (cart.findItemIndexFromCart(itemName) == null)
+                        SizedBox(
+                          width: 165,
+                          height: 45,
+                          child: Container(
+                            child: ElevatedButton(
+                              onPressed: availableToBuy == false
+                                  ? null
+                                  : () {
+                                      Provider.of<CartModel>(
+                                        context,
+                                        listen: false,
+                                      ).addProductToCart(
+                                        itemName,
+                                        itemCost,
+                                        itemName,
+                                      );
+                                    },
+                              style: ElevatedButton.styleFrom(
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                              child: Text(
+                                availableToBuy == false
+                                    ? 'Только в кафе'
+                                    : '$itemCost₽',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 17,
                                 ),
                               ),
-                              Text(
-                                  '${cart.getSpecificItemFromCart(itemName)!.quantity}',
-                                  style: TextStyle(fontSize: 18)),
-                              TextButton(
-                                onPressed: () {
-                                  Provider.of<CartModel>(context, listen: false)
-                                      .incrementItemToCart(itemName);
-                                },
-                                child:
-                                    Text('+', style: TextStyle(fontSize: 18)),
-                              ),
-                            ],
+                            ),
+                          ),
+                        )
+                      else
+                        SizedBox(
+                          width: 165,
+                          height: 45,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(color: Color(0xffFF9F38)),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                TextButton(
+                                  onPressed: () {
+                                    Provider.of<CartModel>(context,
+                                            listen: false)
+                                        .decrementItemFromCart(itemName);
+                                  },
+                                  child: Text(
+                                    '-',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                                Text(
+                                    '${cart.getSpecificItemFromCart(itemName)!.quantity}',
+                                    style: TextStyle(fontSize: 18)),
+                                TextButton(
+                                  onPressed: () {
+                                    Provider.of<CartModel>(context,
+                                            listen: false)
+                                        .incrementItemToCart(itemName);
+                                  },
+                                  child:
+                                      Text('+', style: TextStyle(fontSize: 18)),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
