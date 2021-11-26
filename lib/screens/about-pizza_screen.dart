aae8278ee2ab8b9edecbce8fd43698cfc61f4ae9
Jardynero <egor.vnl@gmail.com>
2021-11-26 19:09:05 +0300

    add analytics tracking

--- lib/screens/about-pizza_screen.dart
+++ lib/screens/about-pizza_screen.dart
@@ -1,5 +1,6 @@
 // About pizza page
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_dym/global_widgets/appBar.dart';
 
@@ -23,6 +24,7 @@ class AboutUs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAnalytics().setCurrentScreen(screenName: 'О нас');
     return ListView(
       children: [
         logo(),
