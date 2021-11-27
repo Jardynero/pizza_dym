55458a6ceffaba9230c65b3203be058e8a76a312
Jardynero <egor.vnl@gmail.com>
2021-11-27 09:57:04 +0300

    add restaurant check opening status

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -2,15 +2,20 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_cart/flutter_cart.dart';
+import 'package:pizza_dym/functions/firebase_functions.dart';
+import 'package:provider/provider.dart';
 
 class CartModel extends ChangeNotifier {
-  
   var cart = FlutterCart();
 
   double get getTotalItemsPrice => cart.getTotalAmount();
 
   void addProductToCart(productId, unitPrice, productName, String imageUrl) {
-    cart.addToCart(productId: productId, unitPrice: unitPrice, productName: productName, productDetailsObject: imageUrl);
+    cart.addToCart(
+        productId: productId,
+        unitPrice: unitPrice,
+        productName: productName,
+        productDetailsObject: imageUrl);
 
     notifyListeners();
   }
@@ -18,7 +23,7 @@ class CartModel extends ChangeNotifier {
   void incrementItemToCart(itemId) {
     cart.incrementItemToCart(cart.findItemIndexFromCart(itemId)!);
 
-    notifyListeners(); 
+    notifyListeners();
   }
 
   void decrementItemFromCart(itemId) {
@@ -27,8 +32,14 @@ class CartModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  void addProductToCartQnt(itemId, unitPrice, productName, quantity, String imageUrl) {
-    cart.addToCart(productId: itemId, unitPrice: unitPrice, productName: productName, quantity: quantity, productDetailsObject: imageUrl);
+  void addProductToCartQnt(
+      itemId, unitPrice, productName, quantity, String imageUrl) {
+    cart.addToCart(
+        productId: itemId,
+        unitPrice: unitPrice,
+        productName: productName,
+        quantity: quantity,
+        productDetailsObject: imageUrl);
 
     notifyListeners();
   }
@@ -40,10 +51,49 @@ class CartModel extends ChangeNotifier {
 
   void obtainQntOfProduct(itemId) {
     try {
-      _singleProductQntInCart =  cart.getSpecificItemFromCart(itemId)!.quantity;
+      _singleProductQntInCart = cart.getSpecificItemFromCart(itemId)!.quantity;
     } catch (e) {
       debugPrint('$e');
     }
+
+    notifyListeners();
+  }
+
+  bool _restaurantStatus = true;
+
+  bool get restaurantStatus => _restaurantStatus;
+
+  String _restaurantStatusMessage = '';
+
+  String get restaurantStatusMessage => _restaurantStatusMessage;
+
+  void checkRestaurantStatus(context) {
+    final bool isOpen =
+        Provider.of<CloudFirestore>(context, listen: false).isOpen;
+
+    final int openingTime =
+        Provider.of<CloudFirestore>(context, listen: false).openingTime;
+
+    final int closingTime =
+        Provider.of<CloudFirestore>(context, listen: false).closingTime;
+
+    final Map workingDays =
+        Provider.of<CloudFirestore>(context, listen: false).workingDays;
+
+    int timeNow = DateTime.now().hour;
+
+    int dayTooday = DateTime.now().weekday;
+
+    if (isOpen == false || timeNow < openingTime || timeNow >= closingTime) {
+      _restaurantStatus = false;
+      _restaurantStatusMessage = 'Мы закрыты';
+    }
+    workingDays.forEach((key, value) {
+      if (key.toString() == dayTooday.toString() && value == false) {
+        _restaurantStatus = false;
+        _restaurantStatusMessage = 'У нас выходной';
+      }
+    });
 
     notifyListeners();
   }
