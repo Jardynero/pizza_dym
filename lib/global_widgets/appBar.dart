f7a456b2a769749cdff8de966af83b41c96ad3e5
Jardynero <egor.vnl@gmail.com>
2021-12-02 02:36:27 +0300

    fix center title

--- lib/global_widgets/appBar.dart
+++ lib/global_widgets/appBar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_dym/global_widgets/appBar-actions.dart';
 
-
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String appBarTitle;
   const MainAppBar(this.appBarTitle, {Key? key}) : super(key: key);
@@ -9,14 +8,23 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('$appBarTitle'),
-      centerTitle: true,
+      titleSpacing: 0,
+      title: Text(
+        '$appBarTitle',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          overflow: TextOverflow.visible,
+        ),
+      ),
+      centerTitle: appBarTitle.length > 18 ? false : true,
       elevation: 4,
       actions: [
         profileAppBarAction(context),
       ],
     );
   }
+
   @override
-  Size get preferredSize =>Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
