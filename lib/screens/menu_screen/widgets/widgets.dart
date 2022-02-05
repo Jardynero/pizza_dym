723cf27b57b0509595ce96e46ce9baf7e8791518
Jardynero <egor.vnl@gmail.com>
2022-02-05 13:15:23 +0300

    init

---
+++ lib/screens/menu_screen/widgets/widgets.dart
@@ -0,0 +1,6 @@
+export 'item-list_screen.dart';
+export 'menuAppBar.dart';
+export 'categorie_heading.dart';
+export 'horizontal_items_list.dart';
+export 'small_item_card.dart';
+export 'vertical_items_list.dart';
