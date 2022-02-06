af052699a5ada14f4b198d1afa828c6b1b2b74f8
Jardynero <egor.vnl@gmail.com>
2022-02-06 16:25:28 +0300

    add check for app update

--- lib/screens/nav_screen/navigation_screen.dart
+++ lib/screens/nav_screen/navigation_screen.dart
@@ -1,8 +1,10 @@
 // Main page
 
 // sys import
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cart/flutter_cart.dart';
 import 'package:pizza_dym/functions/firebase_functions.dart';
 import 'package:pizza_dym/screens/cart_screen/cart_screen.dart';
 
@@ -11,29 +13,52 @@ import 'package:pizza_dym/screens/menu_screen/menu_screen.dart';
 import 'package:pizza_dym/screens/profile/main_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:pizza_dym/screens/nav_screen/widgets/widgets.dart';
+import 'package:update_available/update_available.dart';
 
 class NavigationScreen extends StatefulWidget {
-  const NavigationScreen({Key? key}) : super(key: key);
+  NavigationScreen({Key? key}) : super(key: key);
+  final FlutterCart cart = FlutterCart();
 
   @override
   State<NavigationScreen> createState() => _NavigationScreenState();
 }
 
 class _NavigationScreenState extends State<NavigationScreen> {
+  List<Widget> _screens() {
+    return [MenuScreen(),
+    CartScreen(totalAmount: widget.cart.getTotalAmount().toInt(),),
+    ProfileScreen(),];
+  }
 
-  final List<Widget> _screens = [
-      MenuScreen(),
-      CartScreen(),
-      ProfileScreen(),
-    ];
+  final List<List> _navBarItems = [
+    [Icons.restaurant_outlined, 'Меню'],
+    [Icons.shopping_bag, 'Корзина'],
+    [Icons.person, 'Профиль']
+  ];
 
-    final List<List> _navBarItems = [
-      [Icons.restaurant_outlined, 'Меню'],
-      [Icons.shopping_bag, 'Корзина'],
-      [Icons.person, 'Профиль']
-    ];
+  int _currentIndex = 0;
 
-    int _currentIndex = 0;
+  void updateApp() async {
+    final updateAvailability = await getUpdateAvailability();
+
+    updateAvailability.fold(
+      available: () => Navigator.pushNamedAndRemoveUntil(
+          context, '/update-app', (route) => false),
+      notAvailable: () {
+        debugPrint('No new update for this app');
+      },
+      unknown: () {
+        debugPrint('check for app update result: unknow');
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updateApp();
+  }
+
   @override
   Widget build(BuildContext context) {
     // FirebaseAuth instance
@@ -42,14 +67,12 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
     _auth.setLanguageCode('ru');
 
-
     return Scaffold(
-      body: IndexedStack(index: _currentIndex, children: _screens),
+      body: IndexedStack(index: _currentIndex, children: _screens()),
       bottomNavigationBar: BottomNavBar(
-        items: _navBarItems,
-        selectedIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index)
-      ),
+          items: _navBarItems,
+          selectedIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index)),
     );
   }
 }
